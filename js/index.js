b44ce85fb5d57b73c6d3cf64cb10b14136eab193
onurozcn <ozcan-onur@hotmail.com>
2022-11-03 11:17:36 +0000

    Play sound after keydown or click event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log("Hi new coyote, let's have some JavaScript fun!")
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+function playSound(e) {
+  let code;
+  if (e.keyCode) {
+    // it was a keypress, get the keycode as usual
+    code = e.keyCode;
+  } else {
+    // it was a click.  Read the keycode from the div that was clicked
+    code = this.dataset.key;
+  }
+  const audio = document.querySelector(`audio[data-key="${code}"]`);
+  const key = document.querySelector(`.key[data-key="${code}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+window.addEventListener("keydown", playSound);
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+  key.addEventListener("click", playSound);
+});
